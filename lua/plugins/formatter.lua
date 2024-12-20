92e911d42fbcdfb2cd40df492d110d6acf2a1f66
Jeremy <jeremydunn29@gmail.com>
2024-12-20 13:32:20 -0600

    initial commit

---
+++ lua/plugins/formatter.lua
@@ -0,0 +1,20 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { { "prettierd", "prettier" } },
+				typescript = { { "prettierd", "prettier" } },
+				javascriptreact = { { "prettierd", "prettier" } },
+				typescriptreact = { { "prettierd", "prettier" } },
+				json = { { "prettierd", "prettier" } },
+				graphql = { { "prettierd", "prettier" } },
+				markdown = { { "prettierd", "prettier" } },
+			}
+		})
+	end
+}
