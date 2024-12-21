ff068e978815b294d22956d4f5485be3f8cdcbb6
Jeremy <jeremydunn29@gmail.com>
2024-12-20 22:01:19 -0600

    add hotkeys

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 require("config.lazy")
 
+vim.keymap.set("n", "<space><space>q", function() vim.cmd("q") end)
+
 vim.opt.shiftwidth = 4
 vim.opt.number = true
 vim.opt.relativenumber = true
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,10 +2,36 @@ return {
 	'nvim-telescope/telescope.nvim', tag = '0.1.8',
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
-		require('telescope').setup({
-			extensions = {
-				fzf = {}
-			},
-		})
+	    local t = require("telescope.builtin")
+
+	    -- Open telescope
+	    vim.keymap.set("n", "<space>tt", function() vim.cmd('Telescope') end)
+
+	    -- Open keymaps
+	    vim.keymap.set("n", "<space><space>k", t.keymaps)
+
+	    -- Find Files
+	    vim.keymap.set("n", "<space>ff", t.find_files)
+
+	    -- Find symbol
+	    vim.keymap.set("n", "<space>fs", t.lsp_document_symbols)
+
+	    -- Find references
+	    vim.keymap.set("n", "<space>fr", t.lsp_references)
+
+	    -- "Edit Neovim" - searches neovim config files
+	    vim.keymap.set("n", "<space>en", function ()
+		require('telescope.builtin').find_files {
+		    cwd = vim.fn.stdpath("config")
+		}
+	    end)
+
+	    require('telescope').setup({
+		extensions = {
+		    fzf = {}
+		},
+	    })
+
+	    require('telescope').load_extension('fzf')
 	end
 }
