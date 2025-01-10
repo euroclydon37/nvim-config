27770b9a63445eff6611e87c3f76aa42718527e2
Jeremy <jeremydunn29@gmail.com>
2025-01-10 15:52:08 -0600

    fix autocomplete and highlighting

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,17 +1,15 @@
 return {
 	"saghen/blink.cmp",
+	-- optional: provides snippets for the snippet source
 	dependencies = "rafamadriz/friendly-snippets",
-	version = "0.8.2",
+
+	-- use a release tag to download pre-built binaries
+	version = "*",
+
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
 	opts = {
-		-- 'default' for mappings similar to built-in completion
-		-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-		-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-		-- See the full "keymap" documentation for information on defining your own keymap.
-		keymap = {
-			preset = "default",
-			["<Tab>"] = { "select_and_accept", "fallback" },
-			["<C-:>"] = { "show", "show_documentation", "hide_documentation" },
-		},
+		keymap = { preset = "default" },
 
 		appearance = {
 			-- Sets the fallback highlight groups to nvim-cmp's highlight groups
@@ -20,7 +18,7 @@ return {
 			use_nvim_cmp_as_default = true,
 			-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
 			-- Adjusts spacing to ensure icons are aligned
-			nerd_font_variant = "mono",
+			nerd_font_variant = "normal",
 		},
 
 		-- Default list of enabled providers defined so that you can extend it
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,16 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+
+		configs.setup({
+			modules = {},
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "typescript", "tsx" },
+			ignore_install = {},
+			auto_install = true,
+			sync_install = false,
+			highlight = { enable = true },
+		})
+	end,
 }
